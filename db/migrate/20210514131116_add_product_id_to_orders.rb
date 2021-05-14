7f1ba882db09c8ae25b215c2a389eb9f0c408a47
Bhatara Chaemchan <bhatara007@gmail.com>
2021-05-14 22:31:46 +0700

    fix migration files

--- db/migrate/20210514131116_add_product_id_to_orders.rb
+++ db/migrate/20210514131116_add_product_id_to_orders.rb
@@ -1,7 +1,6 @@
 class AddProductIdToOrders < ActiveRecord::Migration[6.1]
   def change
     add_column :orders, :product_id, :string
-    remove_column :orders, :product
     remove_column :orders, :product_name
   end
 end
